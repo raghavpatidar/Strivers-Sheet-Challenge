09f6a20e8607c73a39065d201c02ea58d497e6e0
raghavpatidar <raghavpatidar997764@gmail.com>
2023-06-07 21:37:24 +0530

    sort 012

---
+++ Arrays/05-sort-012.cpp
@@ -0,0 +1,41 @@
+/*
+┌────────────────────────────────────────────────────────────────────────────────────────┐
+│                      *-----------Raghav Patidar----------*                             |
+|                   Fuck Ratings,Just Enjoy the Contest/Questions                        |
+|                      I'm in Love with Experience and Learning.                         |
+│                                                                                        |
+|    Leetcode   : https://leetcode.com/manito7/                                          |
+|    CodingNinja: https://bit.ly/3IW2Nqi                                                 |
+│    Github     : https://github.com/raghavpatidar                                       |
+|    LinkedIn   : https://www.linkedin.com/in/raghavpatidar07/                           |
+|    Resume     : https://drive.google.com/file/d/1nsTqWKSzxl-tcIXARi_b6vMSVcz57cVQ/view |
+|    Codeforces : https://codeforces.com/profile/raghavpatidar                           |
+|                                                                                        |
+└────────────────────────────────────────────────────────────────────────────────────────┘
+
+Problem : https://www.codingninjas.com/codestudio/problems/sort-0-1-2_8230695?challengeSlug=striver-sde-challenge
+Day :
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#include <bits/stdc++.h> 
+void sort012(int* arr, int n)
+{
+    int low = 0, mid = 0, high = n - 1;
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++, mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
