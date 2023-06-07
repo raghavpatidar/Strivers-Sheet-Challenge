3bfea79c2e31f64dad8b760169db1f25ecd0d5bd
raghavpatidar <raghavpatidar997764@gmail.com>
2023-06-07 21:37:09 +0530

    maximum subarray

---
+++ Arrays/04-maximum-subarray-sum.cpp
@@ -0,0 +1,41 @@
+/*
+┌────────────────────────────────────────────────────────────────────────────────────────┐
+│                      *-----------Raghav Patidar----------*                             |
+|                   Fuck Ratings,Just Enjoy the Contest/Questions                        |
+|                      I'm in Love with Experience and Learning.                         |
+│                                                                                        |
+|    Leetcode   : https://leetcode.com/manito7/                                          |
+|    CodingNinja: https://bit.ly/3IW2Nqi                                                 |
+│    Github     : https://github.com/raghavpatidar                                       |
+|    LinkedIn   : https://www.linkedin.com/in/raghavpatidar07/                           |
+|    Resume     : https://drive.google.com/file/d/1nsTqWKSzxl-tcIXARi_b6vMSVcz57cVQ/view |
+|    Codeforces : https://codeforces.com/profile/raghavpatidar                           |
+|                                                                                        |
+└────────────────────────────────────────────────────────────────────────────────────────┘
+
+Problem : https://www.codingninjas.com/codestudio/problems/maximum-subarray-sum_8230694?challengeSlug=striver-sde-challenge
+Day :
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    /*
+        Don't write main().
+        Don't read input, it is passed as function argument.
+        No need to print anything.
+        Taking input and printing output is handled automatically.
+    */
+    long long int sum = 0, ans = 0;
+    for (int i = 0; i < n;i++) {
+        sum += arr[i];
+        if (sum < 0) {
+            sum = 0;
+        }
+        ans = max(ans, sum);
+    }
+    return ans;
+}
